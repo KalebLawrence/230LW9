6693589e8d19b661c914bf62cd56653ecd3e4bc5
KalebLawrence <ksl00007@mix.wvu.edu>
2024-03-18 23:46:34 -0400

    d

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,25 @@ const db = mysql.createConnection({
 })
 
 app.get("/", (req,res)=>{
-    res.json("hellow this is the backend")
+    res.json("hello this is the backend")
+})
+
+app.get("/books", (req,res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books",(req,res)=>{
+    const q = "INSERT INTO books ('title', 'desc', 'cover') Values (?)"
+    const values = ["title from backend","desc from backend", "cover pic from backend" ]
+
+    db.query(q,[values], (err,data)=>{
+        if(err) return res.json(err)
+        return res.json("Book has been created succesfully")
+    })
 })
 
 app.listen(8800, ()=>{
